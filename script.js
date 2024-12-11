0d3b8b00ddc71cb48b20c645268e0c74ea6147f0
Dasonchris <dasonchris47@gmail.com>
2024-12-11 10:35:57 +0000

    done with responsiveness

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function showSidebar() {
+    const navbar = document.querySelector('.navbar');
+    sidebar.style.display = 'flex';
+  }
+   function hideSidebar(){
+    const navbar = document.querySelector('.navbar');
+    navbar.style.display = 'none';
+   };
+  
