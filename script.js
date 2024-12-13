f2cb77ca4fe368294fbdb221da31d02098e15a1c
Dasonchris <dasonchris47@gmail.com>
2024-12-13 02:49:32 +0000

    added script

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 
+// function showSidebar() {
+//     const navbar = document.querySelector('.navbar');
+//     sidebar.style.display = 'flex';
+//   }
+//    function hideSidebar(){
+//     const navbar = document.querySelector('.navbar');
+//     navbar.style.display = 'none';
+//    };
+
+// hamburger code starts here
 function showSidebar() {
-    const navbar = document.querySelector('.navbar');
-    sidebar.style.display = 'flex';
-  }
-   function hideSidebar(){
-    const navbar = document.querySelector('.navbar');
-    navbar.style.display = 'none';
-   };
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.display = "block";
+}
+
+function hideSidebar() {
+  const hideSidebar = document.querySelector(".sidebar");
+  hideSidebar.style.display = "none";
+}
+
+const navItems = document.querySelectorAll(".navbar-item"); // Select all navbar items
+navItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    hideSidebar(); // Hide the sidebar on click
+  });
+});
+
+// hamburger codes ends here
   
